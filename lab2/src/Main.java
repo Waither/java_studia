be250c50bc0ec3280b8e58bb009802accb90fa2b
maciej.gasior <maciek550@interia.eu>
2024-11-17 11:46:15 +0100

    123

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Color kolor1 = new Color(128, 0, 0, 0.4f);
         Color kolor2 = new Color(255, 128, 52);
-        System.out.println(kolor1.toString());
-        System.out.println(kolor2.toString());
+        System.out.printf("Color: %s, red: %d, green: %d, blue: %d, alpha: %.2f%n", kolor1.toString(), kolor1.red(),kolor1.green(), kolor1.blue(), kolor1.alpha());
+
+        System.out.printf("Color: %s, red: %d, green: %d, blue: %d, alpha: %.2f%n", kolor2.toString(), kolor2.red(),kolor2.green(), kolor2.blue(), kolor2.alpha());
     }
 }
